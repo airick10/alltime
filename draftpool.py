56afc58772364fb251c77f270fa1765d6b17527f
airick10 <58576837+airick10@users.noreply.github.com>
2023-11-04 12:37:56 -0500

    hitters.json & draftpool.py
    Removed blank entry from hitters.json
    Adjusted getDraftPool function in draftpool.py to accept positions.  Close to being finished.

--- draftpool.py
+++ draftpool.py
@@ -21,9 +21,9 @@ def positionList():
 		print("8. Left Fielder")
 		print("9. Center Fielder")
 		print("10. Right Fielder")
-		position_ary[0] = inputcheck.inputCheck("Select what option you want. Type 0 to ignore and go back. ", 1, 10)
+		position_ary[0] = inputcheck.inputCheck("Select what option you want. Type 0 to ignore and go back. ", 0, 10)
 		print("------------")
-		if position_ary[0] > 0:
+		if position_ary[0] > 2:
 			tempchoice = input("Do you want this to be their primary position or at least are eligible there?  Type 'Y' for just the primary and 'N' for anyone eligible ")
 			while tempchoice != "y" and tempchoice != "Y" and tempchoice != "n" and tempchoice != "N":
 				tempchoice = input("Please select 'Y' or 'N'.  Do you want this to be their primary position or at least are eligible there?  Type 'Y' for just the primary and 'N' for anyone eligible ")
@@ -35,7 +35,7 @@ def positionList():
 		else:
 			position_ary[1] = False
 			return position_ary
-		
+
 def getDraftPool(hitters, pitchers, type, sortvalue, selected_list):
 	#X is the number of players displayed at once.
 	X = 30
@@ -54,22 +54,42 @@ def getDraftPool(hitters, pitchers, type, sortvalue, selected_list):
 		position = positionList()
 		if position[0] < 3 and position[0] > 0:
 			type = "P"
-	
+			position[0] = str(position[0])
+			if position[0] == "1":
+				position[0] = "S"
+			else:
+				position[0] = "R"
+		elif position[0] == 0:
+			sortvalue == "None"
+		else:
+			position[0] -= 1
+
 	if type == "H":
 		#for index, player in enumerate(hitters[:X]):
-		while listcounter < 31:	
+		while listcounter < 31 and len(hitters) > counter:	
 			#Look for players who have not been drafted yet
 			if hitters[counter]['ID'] not in selected_list:
-				choicearray = getDraftPoolList(hitters[counter]['ID'], hitters[counter]['FirstName'], hitters[counter]['LastName'], hitters[counter]['Year'], hitters[counter]['Team'], choicearray, listcounter)
-				listcounter += 1
+				position[0] = str(position[0])
+				if sortvalue == "Position":
+					if hitters[counter]['DP1'] == position[0]:
+						choicearray = getDraftPoolList(hitters[counter]['ID'], hitters[counter]['FirstName'], hitters[counter]['LastName'], hitters[counter]['Year'], hitters[counter]['Team'], choicearray, listcounter)
+						listcounter += 1
+				else:
+					choicearray = getDraftPoolList(hitters[counter]['ID'], hitters[counter]['FirstName'], hitters[counter]['LastName'], hitters[counter]['Year'], hitters[counter]['Team'], choicearray, listcounter)
+					listcounter += 1
 			counter += 1
 	else:
 		#for index, player in enumerate(pitchers[:X]):
-		while listcounter < 31:
+		while listcounter < 31 and len(pitchers) > counter:
 			#Look for players who have not been drafted yet
 			if pitchers[counter]['ID'] not in selected_list:
-				chiocearray = getDraftPoolList(pitchers[counter]['ID'], pitchers[counter]['FirstName'], pitchers[counter]['LastName'], pitchers[counter]['Year'], pitchers[counter]['Team'], choicearray, listcounter)
-				listcounter += 1
+				if sortvalue == "Position":
+					if pitchers[counter]['Role'] == position[0]:
+						chiocearray = getDraftPoolList(pitchers[counter]['ID'], pitchers[counter]['FirstName'], pitchers[counter]['LastName'], pitchers[counter]['Year'], pitchers[counter]['Team'], choicearray, listcounter)
+						listcounter += 1
+				else:
+					choicearray = getDraftPoolList(pitchers[counter]['ID'], pitchers[counter]['FirstName'], pitchers[counter]['LastName'], pitchers[counter]['Year'], pitchers[counter]['Team'], choicearray, listcounter)
+					listcounter += 1
 			counter += 1
 	
 	choice = inputcheck.inputCheck("Select the number for a player you want to see more details of.  Select 0 to go back to the menu.", 0, 30)
